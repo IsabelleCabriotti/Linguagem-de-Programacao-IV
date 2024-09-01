1dfddd9512317e67f1f0ada666f2e9910477f5b4
IsabelleCabriotti <isahcabriotti@gmail.com>
2024-09-01 00:37:54 -0300

    Lista2

---
+++ Eletiva I/Lista2/exer7.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 7</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="container">
+    <h1>Exercício 7</h1>
+    <form action="exer7rp.php" method="POST">
+        <div class="row">
+            <div class="col">
+                <label for="fahrenheit" class="form-label">Informe a temperatura em Fahrenheit: </label>
+                <input type="number" class="form-control" step="0.1" name="fahrenheit" id="fahrenheit">
+            </div>
+        </div>
+        <div class="row mt-2">
+            <div class="col">
+                <button type="submit" class="btn btn-primary">Converte para Celsius</button>
+            </div>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
